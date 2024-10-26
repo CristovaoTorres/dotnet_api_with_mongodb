c544a294de39f330e7e69f0342a395a75f9f7150
Cristóvão Torres <Cristovao.ftorres@hotmail.com>
2024-10-26 20:08:12 -0300

    Incluindo camada CrossCutting

---
+++ DotNetExample_API.CrossCutting/DependencyInjectionConfig.cs
@@ -0,0 +1,16 @@
+﻿using DotNetExample.Business;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotNetExample_API.CrossCutting
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void AddDependencyInjection(this IServiceCollection services)
+        {
+            // Registrar as dependências globais
+            services.AddScoped<IProdutoBusiness, ProdutoBusiness>();
+            services.AddSingleton<IProductRepository, ProductRepository>();
+            // Outras configurações de dependências
+        }
+    }
+}
